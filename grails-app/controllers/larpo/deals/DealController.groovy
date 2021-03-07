cb889bac6c4439f732ac105a6e04af2df152be04
Julien LATULIPE <julien.latulipe@epita.fr>
2021-03-07 02:06:39 +0100

    LD: add unit test, find my deal complete!

--- grails-app/controllers/larpo/deals/DealController.groovy
+++ grails-app/controllers/larpo/deals/DealController.groovy
@@ -4,17 +4,21 @@ class DealController
 {
     // def index() { }
 
+   // def list()
    def list(params)
     {
         // List<Deal> deals = Deal.list(params)
-        println("Hello World")
+        // println("Hello World")
         def c = Deal.createCriteria()
         def deals = c.list()
         {
             // println (params)
-            if (params.search)
-                ilike("name", params.search)
+            if (params.search) {
+                // println(params.search) //FIME DEBUG PRINT
+                ilike("name", "%" + params.search + "%")
+            }
         }
+        // println(deals) //FIME DEBUG PRINT
         [deals: deals]
     }
 }
---
+++ src/test/groovy/larpo/deals/DealControllerSpec.groovy
@@ -0,0 +1,57 @@
+package larpo.deals
+
+import grails.testing.gorm.DataTest
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class DealControllerSpec extends Specification implements ControllerUnitTest<DealController>, DataTest {
+
+    @Override
+    Class[] getDomainClassesToMock()
+    {
+        return [Deal]
+    }
+
+    def setupData()
+    {
+        new BootStrap().init.run()
+    }
+
+    def setup()
+    {
+    }
+
+    def cleanup()
+    {
+    }
+
+    /*void "test something"() {
+        expect:"fix me"
+            true == false
+    }*/
+
+    void "empty search" ()
+    {
+        given: setupData(); params.search = null
+        when: def res = controller.list(params)
+        then: res.deals.size() == 3
+    }
+
+    void "good Bus search" ()
+    {
+        given: setupData(); params.search = "Bus"
+        when: def res = controller.list(params)
+        then:
+            res.deals.size() == 2
+            for (e in res.deals)
+            {
+                assert e.name == "Bus"
+            }
+    }
+    void "bad Bike search" ()
+    {
+        given: setupData(); params.search = "Bike"
+        when: def res = controller.list(params)
+        then: res.deals.size() == 0
+    }
+}
